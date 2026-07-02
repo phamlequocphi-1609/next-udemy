4207da4fda08740fec7a504d4a8f06f37d4c627b
quocphi1609 <phamlequocphi16092001@gmail.com>
2026-07-02 20:39:22 +0700

    passing Req.user via service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { CompaniesService } from './companies/companies.service';
-import { CreateCompanyDto } from './companies/dto/create-company.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly companyService: CompaniesService) {}
-
-  @Post('/companies')
-  createCompany(@Body() createCompanyDto: CreateCompanyDto) {
-    return this.companyService.create(createCompanyDto);
-  }
+  constructor() {}
 }
--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -4,6 +4,7 @@ import { UpdateCompanyDto } from './dto/update-company.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Company, CompanyDocument } from './schema/company.schema';
 import type { SoftDeleteModel } from 'soft-delete-plugin-mongoose/dist/src';
+import { IUser } from 'src/users/users.interface';
 
 @Injectable()
 export class CompaniesService {
@@ -12,8 +13,14 @@ export class CompaniesService {
     private companyModel: SoftDeleteModel<CompanyDocument>,
   ) {}
 
-  create(createCompanyDto: CreateCompanyDto) {
-    return this.companyModel.create({ ...createCompanyDto });
+  create(createCompanyDto: CreateCompanyDto, user: IUser) {
+    return this.companyModel.create({
+      ...createCompanyDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
+    });
   }
 
   findAll() {
