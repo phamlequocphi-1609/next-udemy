7920ef7cb32627be86e115cc4f025115e3a35534
quocphi1609 <phamlequocphi16092001@gmail.com>
2026-07-06 20:41:56 +0700

    feat: implement update schema user

--- src/companies/dto/update-company.dto.ts
+++ src/companies/dto/update-company.dto.ts
@@ -1,19 +1,21 @@
 import { IsNotEmpty } from 'class-validator';
+import { CreateCompanyDto } from './create-company.dto';
 
 // data transfer object
-export class UpdateCompanyDto {
-  @IsNotEmpty({
-    message: 'Name không được để trống',
-  })
-  name: string;
+export class UpdateCompanyDto extends CreateCompanyDto {}
+// export class UpdateCompanyDto {
+//   @IsNotEmpty({
+//     message: 'Name không được để trống',
+//   })
+//   name: string;
 
-  @IsNotEmpty({
-    message: 'Address không đươc để trống',
-  })
-  address: string;
+//   @IsNotEmpty({
+//     message: 'Address không đươc để trống',
+//   })
+//   address: string;
 
-  @IsNotEmpty({
-    message: 'Description không đươc để trống',
-  })
-  description: string;
-}
+//   @IsNotEmpty({
+//     message: 'Description không đươc để trống',
+//   })
+//   description: string;
+// }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,28 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  ValidateNested,
+} from 'class-validator';
+import mongoose from 'mongoose';
 
 // data transfer object
+
+export class Company {
+  @IsNotEmpty()
+  _id: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty()
+  name: string;
+}
 export class CreateUserDto {
+  @IsNotEmpty({
+    message: 'Name không đươc để trống',
+  })
+  name: string;
+
   @IsEmail(
     {},
     {
@@ -18,6 +39,67 @@ export class CreateUserDto {
   })
   password: string;
 
+  @IsNotEmpty({
+    message: 'Age không được để trống',
+  })
+  age: number;
+
+  @IsNotEmpty({
+    message: 'Gender không được để trống',
+  })
+  gender: string;
+
+  @IsNotEmpty({
+    message: 'Addess không được để trống',
+  })
+  address: string;
+
+  @IsNotEmpty({
+    message: 'Role không được để trống',
+  })
+  role: string;
+
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
+}
+
+export class RegisterUserDto {
+  @IsNotEmpty({
+    message: 'Name không đươc để trống',
+  })
   name: string;
+
+  @IsEmail(
+    {},
+    {
+      message: 'Email không đúng định dạng',
+    },
+  )
+  @IsNotEmpty({
+    message: 'Email không được để trống',
+  })
+  email: string;
+
+  @IsNotEmpty({
+    message: 'Password không đươc để trống',
+  })
+  password: string;
+
+  @IsNotEmpty({
+    message: 'Age không được để trống',
+  })
+  age: number;
+
+  @IsNotEmpty({
+    message: 'Gender không được để trống',
+  })
+  gender: string;
+
+  @IsNotEmpty({
+    message: 'Addess không được để trống',
+  })
   address: string;
 }
