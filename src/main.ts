807ba8dd73f3f1ef7dcdacc8e2ac42582fc7670e
quocphi1609 <phamlequocphi16092001@gmail.com>
2026-07-18 23:11:43 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -30,9 +30,10 @@ async function bootstrap() {
 
   //config cors
   app.enableCors({
-    origin: '*',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
+    credentials: true,
   });
 
   //config tiền tố global api
