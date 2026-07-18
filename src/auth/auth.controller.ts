9b89da918d16cd13475ef902bf83e38867e20d50
quocphi1609 <phamlequocphi16092001@gmail.com>
2026-07-18 20:42:04 +0700

    implement get user info

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,11 +8,11 @@ import {
   Req,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Public, ResponseMessage } from 'src/decorator/customize';
+import { Public, ResponseMessage, User } from 'src/decorator/customize';
 import { LocalAuthGuard } from './local-auth.guard';
 import { RegisterUserDto } from 'src/users/dto/create-user.dto';
 import type { Request, Response } from 'express';
-import { IUser } from 'src/users/users.interface';
+import type { IUser } from 'src/users/users.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -40,5 +40,13 @@ export class AuthController {
   @Post('/register')
   handleRegister(@Body() regiserUserDto: RegisterUserDto) {
     return this.authService.register(regiserUserDto);
+  }
+
+  @ResponseMessage('Get user information')
+  @Get('/account')
+  handleGetAccount(@User() user: IUser) {
+    return {
+      user,
+    };
   }
 }
