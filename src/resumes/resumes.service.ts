77bbc12bea8ecead5bafbc6f7bbc5144bcdc187f
quocphi1609 <phamlequocphi16092001@gmail.com>
2026-07-21 16:12:10 +0700

    implement fetch data with Ref

--- src/resumes/resumes.service.ts
+++ src/resumes/resumes.service.ts
@@ -1,26 +1,132 @@
-import { Injectable } from '@nestjs/common';
-import { CreateResumeDto } from './dto/create-resume.dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserCvDto } from './dto/create-resume.dto';
 import { UpdateResumeDto } from './dto/update-resume.dto';
+import { IUser } from 'src/users/users.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Resume, ResumeDocument } from './schema/resume.schema';
+import type { SoftDeleteModel } from 'soft-delete-plugin-mongoose/dist/src';
+import mongoose from 'mongoose';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class ResumesService {
-  create(createResumeDto: CreateResumeDto) {
-    return 'This action adds a new resume';
+  constructor(
+    @InjectModel(Resume.name)
+    private resumeModel: SoftDeleteModel<ResumeDocument>,
+  ) {}
+
+  async create(createUserCvDto: CreateUserCvDto, user: IUser) {
+    const { companyId, jobId, url } = createUserCvDto;
+    const { _id, email } = user;
+    const newCv = await this.resumeModel.create({
+      url,
+      companyId,
+      email,
+      jobId,
+      userId: _id,
+      status: 'PENDING',
+      createdBy: { _id, email },
+      history: [
+        {
+          status: 'PENDING',
+          updatedAt: new Date(),
+          updatedBy: {
+            _id: user._id,
+            email: user.email,
+          },
+        },
+      ],
+    });
+    return {
+      _id: newCv?._id,
+      createdAt: newCv?.createdAt,
+    };
   }
 
-  findAll() {
-    return `This action returns all resumes`;
+  async findAll(currentPage: number, limit: number, qs: string) {
+    const { filter, sort, population, projection } = aqp(qs);
+    delete filter.current;
+    delete filter.pageSize;
+
+    let defaultLimit = +limit ? +limit : 10;
+    let current = +currentPage ? +currentPage : 1;
+    let offset = (current - 1) * defaultLimit;
+
+    const totalItems = (await this.resumeModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.resumeModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .select(projection as any)
+      .exec();
+
+    return {
+      meta: {
+        current: current,
+        pageSize: defaultLimit,
+        pages: totalPages,
+        total: totalItems,
+      },
+      result,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} resume`;
+  async findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('not found resume');
+    }
+    return await this.resumeModel.findById(id);
   }
 
-  update(id: number, updateResumeDto: UpdateResumeDto) {
-    return `This action updates a #${id} resume`;
+  async findByUsers(user: IUser) {
+    return await this.resumeModel.find({ userId: user._id });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} resume`;
+  async update(id: string, status: string, user: IUser) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('not found resume');
+    }
+
+    const updated = await this.resumeModel.updateOne(
+      { _id: id },
+      {
+        status,
+        updatedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+        $push: {
+          history: {
+            status: status,
+            updatedAt: new Date(),
+            updatedBy: {
+              _id: user._id,
+              email: user.email,
+            },
+          },
+        },
+      },
+    );
+    return updated;
+  }
+
+  async remove(id: string, user: IUser) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('this resume not found');
+    }
+    await this.resumeModel.updateOne(
+      { _id: id },
+      {
+        deletedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+    return this.resumeModel.softDelete({ _id: id });
   }
 }
